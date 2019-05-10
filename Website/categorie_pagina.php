0b7ea617a7d3c2349fb21f1ae979539260a0cc47
BeingBright <BrightonvandenEnd@gmail.com>
2019-05-10 14:55:50 +0200

    Caregorie Pagina Fix

--- Website/categorie_pagina.php
+++ Website/categorie_pagina.php
@@ -1,5 +1,5 @@
 <?php
-include_once ("database.php");
+include_once("database.php");
 include_once("header.php");
 $categorieArtikel = '<div class="container" >';
 
@@ -11,48 +11,43 @@ $categorieArtikel = ' <div class="container" style="background-color: #4b4c4d;">
 while ($rij = $hoofdcategorieData->fetch()) {
     $hoofdcategorie = $rij['Rubrieknaam'];
 
-    $categorieArtikel .= '<div class="row text-capitalize text-left" style="height: 275px;">';
-$categorieArtikel.= '<div class="col-4 col-lg-3" style="color: rgb(255,255,255);background-color: #3a3a3a;">
+    $categorieArtikel .= '<div class="row text-capitalize text-left" style="">';
+    $categorieArtikel .= '<div class="col-4 col-lg-3" style="color: rgb(255,255,255);background-color: #3a3a3a;">
                 <a href="#" style="color: #ffffff;">
-                    <h1 style="color: #ffffff;">' . $rij['Rubrieknaam'] . '</h1>
+                    <h1 style="color: #ffffff; word-wrap:break-word;">' . $rij['Rubrieknaam'] . '</h1>
                 </a>
             </div>
-            <div class="col-2 col-lg-3 d-table-cell row">
-                <div class="row" style="background-image: url(&quot;assets/img/band.jpg&quot;);height: 80%;background-position: center;background-size: cover;background-repeat: no-repeat;">
-                    <div class="col">
-                        <header></header>
-                    </div>
-                </div>
+            <div class="col-2 col-lg-3 d-table-cell row" ">
+                <img src="assets/img/band.jpg" alt="Smiley face" height="150" width="150">
             </div> <div class="col" style="background-color: #3a3a3a;">';
 
     $subcategorie = $dbh->prepare('SELECT Rubrieknaam FROM Rubriek WHERE Parent_Rubriek = :Rubrieknummer ORDER BY Volgnr, Rubrieknaam ');
     $subcategorie->execute(['Rubrieknummer' => $rij["Rubrieknummer"]]);
 
-    while($rij = $subcategorie->fetch()){
-    $subcategorienaam = $rij['Rubrieknaam'];
+    while ($rij = $subcategorie->fetch()) {
+        $subcategorienaam = $rij['Rubrieknaam'];
 
-    $categorieArtikel .= '<div class="row col" style="margin-bottom: 5px;"> <a href="#" style="color: #ffffff;">
+        $categorieArtikel .= '<div class="row col" style="margin-bottom: 5px;"> <a href="?Categorie=' . $subcategorienaam . '" style="color: #ffffff;">
                         <h6>' . $subcategorienaam . '</h6>
                     </a> </div>';
     }
-   $categorieArtikel .= ' </div>';
     $categorieArtikel .= ' </div>';
-
+    $categorieArtikel .= ' </div>';
 }
 
-$categorieArtikel .='</div>';
+$categorieArtikel .= '</div>';
 
 
 ?>
 <html>
-<body class="background"">
-<?php echo $categorieArtikel ?>
-<script src="assets/js/jquery.min.js"></script>
-<script src="assets/bootstrap/js/bootstrap.min.js"></script>
+
+<body class="background">
+    <?php echo $categorieArtikel ?>
+    <script src=" assets/js/jquery.min.js"> </script>
+    <script src="assets/bootstrap/js/bootstrap.min.js">
+    </script>
 </body>
+
 </html>
 
 <?php include_once("footer.php"); ?>
-
-
-
