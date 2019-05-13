6791aef08e01363e842c7799e5c4d3f2a06e54c3
AaronThunnissen <49440443+AaronThunnissen@users.noreply.github.com>
2019-05-13 11:42:21 +0200

    Add files via upload

--- Website/footer.php
+++ Website/footer.php
@@ -5,15 +5,15 @@
     <footer>
         <div class="container">
             <div class="row">
-                <div class="col-sm-6 col-md-3 item">
+                <div class="col-sm-6">
                     <h3>Services</h3>
                     <ul>
                         <li><a href="#">Veilingen</a></li>
                         <li><a href="#">Over Ons</a></li>
-                        <li><a href="#">Rubrieken</a></li>
+                        <li><a href="rubrieken.php">Rubrieken</a></li>
                     </ul>
                 </div>
-                <div class="col-md-6 item text">
+                <div class="col-sm-6">
                     <h3>EenmaalAndermaal</h3>
                     <p>Praesent sed lobortis mi. Suspendisse vel placerat ligula. Vivamus ac sem lacus. Ut vehicula rhoncus elementum. Etiam quis tristique lectus. Aliquam in arcu eget velit pulvinar dictum vel in justo.</p>
                 </div>
