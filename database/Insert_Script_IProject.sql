574f167c983c1ec8df69ee80f1b25d5d88678467
JosseNobel <49642531+JosseNobel@users.noreply.github.com>
2019-05-13 09:39:16 +0200

    Delete Insert_Script_IProject.sql

--- database/Insert_Script_IProject.sql
+++
@@ -1,8 +0,0 @@
-USE EenmaalAndermaal
-GO
-
-INSERT Vraag VALUES ('Wat is de naam van je geboorteplaats?')
-INSERT Vraag VALUES ('Wat is de naam van je eerste huisdier?')
-INSERT Vraag VALUES ('Wat is de naam van moeder?')
-INSERT Vraag VALUES ('Wat is de naam van je favoriete band?')
-INSERT Vraag VALUES ('Wat is je favoriete game?')
