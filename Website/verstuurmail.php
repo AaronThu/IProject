79b1566c7dea0de3c6527a9a3b68e9d6d114fdd8
Wouter Breedveld <wouter.rbreedveld@gmail.com>
2019-05-14 13:50:24 +0200

    Update verstuurmail.php

--- Website/verstuurmail.php
+++ Website/verstuurmail.php
@@ -1,5 +1,5 @@
 <?php
-include_once ('functies.php');
+include_once('functies.php');
 session_start();
 $locatieNaVersturen = "Location: http://iproject2.icasites.nl/index.php";
 
@@ -7,28 +7,23 @@ $mailadres = $_SESSION['Emailadres'];
 $masterPW = "test";
 $hash = hash('sha256', $mailadres . $masterPW);
 
-$add =  $mailadres .'&hash='.$hash. '&tijd='. time();
-$to      = $mailadres;
+$add = $mailadres . '&hash=' . $hash . '&tijd=' . time();
+$to = $mailadres;
 $subject = 'Registreren | Verificatie';
-$message = "
- 
-hoi,
-Bedankt voor het registreren!
-Klik hieronder op de link om jouw mailadres te verifieren en om de registratie te kunnen voortzetten:
+$message = '<html lang="nl"><body>';
+$message .= '<p>Hello, <br> hier onder staat je link!</p>';
+$message .='http://iproject2.icasites.nl/checkemail.php?email='. $mailadres .'&hash='.$hash. '&tijd='. time();
+$message .= '</body></html>';
 
- --------------------------------------------------------------
- (deze link is maar 4 uur geldig)
- 
-http://iproject2.icasites.nl/checkemail.php?email=$add".
-
-'a alvast bedankt
-EenmaalAndermaal team';
 
 $headers = 'From:noreply@EenmaalAndermaal.com' . "\r\n";
+$headers = 'MIME-Version:1.0' . "\r\n";
 mail($to, $subject, $message, $headers);
-
 
 
 $_SESSION['foutmelding'] = "Mail is succesvol verstuurd, kijk in je inbox voor de link!";
 header($locatieNaVersturen);
 ?>
+
+
+
