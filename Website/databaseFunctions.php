a75cef1081324c3067943d5d0ba45e3ebd696db5
BeingBright <BrightonvandenEnd@gmail.com>
2019-05-15 11:52:57 +0200

    Rubrieken pagina styling fix

--- Website/databaseFunctions.php
+++ Website/databaseFunctions.php
@@ -1,6 +1,5 @@
 <?php
-function GetRubrieken($parent)
-{
+function GetRubrieken($parent) {
     global $dbh;
     $mainCategoriesQuery = $dbh->prepare("SELECT Rubrieknaam, Rubrieknummer FROM Rubriek WHERE Parent_Rubriek = ? ORDER BY Volgnr, Rubrieknaam");
     $mainCategoriesQuery->execute([$parent]);
@@ -8,12 +7,23 @@ function GetRubrieken($parent)
     return $mainCategories;
 }
 
-function GetRubriekenPopulair($max)
-{
+function GetRubriekenPopulair($max) {
     global $dbh;
     $mainCategoriesQuery = $dbh->prepare("SELECT Rubrieknaam, Rubrieknummer FROM Rubriek WHERE Parent_Rubriek = -1 ORDER BY Volgnr, Rubrieknaam");
     $mainCategoriesQuery->execute();
     $mainCategories = $mainCategoriesQuery->fetchAll();
     return array_chunk($mainCategories, $max)[0];
+}
+
+function GetRubriekenFoto($id) {
+    global $dbh;
+    $fotoQuery = $dbh->prepare("SELECT RubriekFoto FROM RubriekFotos WHERE Rubrieknummer=?;");
+    $fotoQuery->execute([$id]);
+    $fotoPath = $fotoQuery->fetchAll();
+    if (isset($fotoPath[0]) and isset($fotoPath[0][0])) {
+        return "assets/img/Rubrieken/" . $fotoPath[0][0];
+    } else {
+        return null;
+    }
 }
 ?>
--- Website/rubrieken.php
+++ Website/rubrieken.php
@@ -1,55 +1,68 @@
 <?php
-include_once("database.php");
-include_once("header.php");
-$categorieArtikel = '<div class="container" >';
+include_once "database.php";
+include_once "header.php";
+include_once "databaseFunctions.php";
 
+// $categorieArtikel = '<div class="container" >';
 
+// $hoofdcategorieData = $dbh->query("SELECT Rubrieknaam, Rubrieknummer FROM Rubriek WHERE Parent_Rubriek = -1 ORDER BY Volgnr, Rubrieknaam");
 
-$hoofdcategorieData = $dbh->query("SELECT Rubrieknaam, Rubrieknummer FROM Rubriek WHERE Parent_Rubriek = -1 ORDER BY Volgnr, Rubrieknaam");
+// $categorieArtikel = ' <div class="container" style="background-color: #4b4c4d;">';
+// while ($rij = $hoofdcategorieData->fetch()) {
+//     $hoofdcategorie = $rij['Rubrieknaam'];
+//     $Rubrieknummer = $rij['Rubrieknummer'];
 
-$categorieArtikel = ' <div class="container" style="background-color: #4b4c4d;">';
-while ($rij = $hoofdcategorieData->fetch()) {
-    $hoofdcategorie = $rij['Rubrieknaam'];
-    $Rubrieknummer = $rij['Rubrieknummer'];
+//     $hoofdcategorieFoto = $dbh->prepare("SELECT RubriekFoto FROM RubriekFotos WHERE Rubrieknummer = :Rubrieknummer");
+//     $hoofdcategorieFoto->execute([
+//         ':Rubrieknummer' => $Rubrieknummer
+//     ]);
+//     $foto = $hoofdcategorieFoto->fetchColumn();
 
-    $hoofdcategorieFoto = $dbh->prepare("SELECT RubriekFoto FROM RubriekFotos WHERE Rubrieknummer = :Rubrieknummer");
-    $hoofdcategorieFoto->execute([
-        ':Rubrieknummer' => $Rubrieknummer
-    ]);
-    $foto = $hoofdcategorieFoto->fetchColumn();
+//     $categorieArtikel .= '<div class="row text-capitalize text-left" style="">';
+//     $categorieArtikel .= '<div class="col-4 col-lg-3" style="color: rgb(255,255,255);background-color: #3a3a3a;">
+//                 <a href="#" style="color: #ffffff;">
+//                     <h1 style="color: #ffffff; word-wrap:break-word;">' . $rij['Rubrieknaam'] . '</h1>
+//                 </a>
+//             </div>
+//             <div class="col-2 col-lg-3 d-table-cell row">
+//                 <img src="assets/img/Rubrieken/' . $foto .'" alt="' . $foto . '" height="250" width="250">
+//             </div> <div class="col" style="background-color: #3a3a3a;">';
 
-    $categorieArtikel .= '<div class="row text-capitalize text-left" style="">';
-    $categorieArtikel .= '<div class="col-4 col-lg-3" style="color: rgb(255,255,255);background-color: #3a3a3a;">
-                <a href="#" style="color: #ffffff;">
-                    <h1 style="color: #ffffff; word-wrap:break-word;">' . $rij['Rubrieknaam'] . '</h1>
-                </a>
-            </div>
-            <div class="col-2 col-lg-3 d-table-cell row">
-                <img src="assets/img/Rubrieken/' . $foto .'" alt="' . $foto . '" height="250" width="250">
-            </div> <div class="col" style="background-color: #3a3a3a;">';
+//     $subcategorie = $dbh->prepare('SELECT Rubrieknaam FROM Rubriek WHERE Parent_Rubriek = :Rubrieknummer ORDER BY Volgnr, Rubrieknaam ');
+//     $subcategorie->execute(['Rubrieknummer' => $rij["Rubrieknummer"]]);
 
-    $subcategorie = $dbh->prepare('SELECT Rubrieknaam FROM Rubriek WHERE Parent_Rubriek = :Rubrieknummer ORDER BY Volgnr, Rubrieknaam ');
-    $subcategorie->execute(['Rubrieknummer' => $rij["Rubrieknummer"]]);
+//     while ($rij = $subcategorie->fetch()) {
+//         $subcategorienaam = $rij['Rubrieknaam'];
 
-    while ($rij = $subcategorie->fetch()) {
-        $subcategorienaam = $rij['Rubrieknaam'];
+//         $categorieArtikel .= '<div class="row col" style="margin-bottom: 5px;"> <a href="?Categorie=' . $subcategorienaam . '" style="color: #ffffff;">
+//                         <h6>' . $subcategorienaam . '</h6>
+//                     </a> </div>';
+//     }
+//     $categorieArtikel .= ' </div>';
+//     $categorieArtikel .= ' </div>';
+// }
 
-        $categorieArtikel .= '<div class="row col" style="margin-bottom: 5px;"> <a href="?Categorie=' . $subcategorienaam . '" style="color: #ffffff;">
-                        <h6>' . $subcategorienaam . '</h6>
-                    </a> </div>';
-    }
-    $categorieArtikel .= ' </div>';
-    $categorieArtikel .= ' </div>';
-}
-
-$categorieArtikel .= '</div>';
-
+// $categorieArtikel .= '</div>';
 
 ?>
     <html>
 
     <body class="background">
-    <?php echo $categorieArtikel ?>
+        <div class="Main">
+    <?php
+foreach (GetRubrieken(-1) as $key => $value) {
+    print("<div class=\"container rubriek\">");
+    print("<h1 class=\"item\" style=\"width: 10em;\">$value[Rubrieknaam]</h1>");
+    print("<div class=\"item background image\"><img src=\"" . GetRubriekenFoto($value["Rubrieknummer"]) . "\"></div>");
+    print("<div class=\"item\">");
+    foreach (GetRubrieken($value["Rubrieknummer"]) as $key => $value) {
+        print("<a href=\"#\">$value[Rubrieknaam]</a>");
+    }
+    print("</div>");
+    print("</div>");
+}
+?>
+</div>
     <script src=" assets/js/jquery.min.js"> </script>
     <script src="assets/bootstrap/js/bootstrap.min.js">
     </script>
@@ -57,4 +70,4 @@ $categorieArtikel .= '</div>';
 
     </html>
 
-<?php include_once("footer.php"); ?>
+<?php include_once "footer.php";?>
