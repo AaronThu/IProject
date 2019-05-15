947efaf6e84eb33300c0fecd33b3cd2c7f9c1270
BeingBright <BrightonvandenEnd@gmail.com>
2019-05-15 14:56:35 +0200

    Create subrubrieken_pagina.php

---
+++ Website/subrubrieken_pagina.php
@@ -0,0 +1,21 @@
+<?php
+include_once "database.php";
+include_once "functies.php";
+include_once "header.php";
+?>
+    <html>
+    <body class="background">
+
+
+
+    <script src="assets/js/jquery.min.js"></script>
+    <script src="assets/bootstrap/js/bootstrap.min.js"></script>
+    <script src="assets/js/timer.js"></script>
+    </body>
+
+
+    </html>
+
+
+<?php
+include_once "footer.php";?>
