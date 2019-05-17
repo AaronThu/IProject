f756122dd7a172a5cc00393fafb84d50e63fe660
Tymo <tymoschotanus@live.nl>
2019-05-17 10:25:31 +0200

    update banner

--- Website/includes/banner.php
+++ Website/includes/banner.php
@@ -6,7 +6,7 @@
     <div class="flex-sm-column flex-md-row row">
 <img src="assets/img/vinkjeBanner.png" width="30" height="30" style="margin-left: 1em"><h6>Registreer binnen 1 minuut</h6>
         <img class="bannerfotos" src="assets/img/vinkjeBanner.png"  width="30" height="30" style="margin-left: 2em"><h6 >Krijg toegang tot TOP artikelen voor een TOP prijs</h6>
-        <img src="assets/img/vinkjeBanner.png" width="30" height="30" style="margin-left: 2em"><h6>Biedt mee op <?php TelVoorwerpen(); ?> artikelen</h6>
+        <img src="assets/img/vinkjeBanner.png" width="30" height="30" style="margin-left: 2em"><h6>Biedt mee op DUIZENDEN artikelen</h6>
     </div>
 </div>
     <span class="d-flex justify-content-center align-items-center navbar-text actions bannerknop"><a class="btn btn-primary action-button" style="background-color: #ffb357" data-bs-hover-animate="pulse" role="button" href="registreren_emailpagina.php">Registreer nu!</a></span>
