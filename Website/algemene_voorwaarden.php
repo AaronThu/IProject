1226b27e9453589629eb25e38e4f940984357491
Tymo <tymoschotanus@live.nl>
2019-05-24 14:10:24 +0200

    Create algemene_voorwaarden.php

---
+++ Website/algemene_voorwaarden.php
@@ -0,0 +1,32 @@
+<?php
+include_once "includes/header.php";
+
+?>
+    <body class="background">
+
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <h2 class="titel" style="text-align: center";>Algemene Voorwaarden</h2><br>
+            </div>
+        </div>
+        <div class="row">
+        <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean imperdiet. Etiam ultricies nisi vel augue. Curabitur ullamcorper ultricies nisi. Nam eget dui.</p>
+        </div>
+    </div>
+    
+
+
+    <script src="assets/js/jquery.min.js"></script>
+    <script src="assets/bootstrap/js/bootstrap.min.js"></script>
+    <script src="assets/js/timer.js"></script>
+    
+
+    </body>
+
+
+
+
+
+<?php
+include_once "includes/footer.php";?>
