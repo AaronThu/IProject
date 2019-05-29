30215a4d813036a768659f8ae1b7e099e54c9a96
AaronThunnissen <49440443+AaronThunnissen@users.noreply.github.com>
2019-05-29 10:32:57 +0200

    Update databaseFunctions.php

--- Website/includes/databaseFunctions.php
+++ Website/includes/databaseFunctions.php
@@ -77,7 +77,7 @@ function GetProductNaam($id) {
 function GetMeerVanVerkoper($id) {
     global $dbh;
     $voorwerpnummer = $id;
-    $MeerVanVerkoperQuery = $dbh->prepare("SELECT TOP 4 v.Voorwerpnummer, b.FileNaam FROM Voorwerp v INNER JOIN Bestand b ON v.Voorwerpnummer = b.VoorwerpNummer INNER JOIN Verkoper ver ON v.VerkopersID = ver.GebruikersID WHERE v.Voorwerpnummer != $voorwerpnummer and VerkopersID IN
+    $MeerVanVerkoperQuery = $dbh->prepare("SELECT DISTINCT TOP 4 v.Voorwerpnummer, b.FileNaam FROM Voorwerp v INNER JOIN Bestand b ON v.Voorwerpnummer = b.VoorwerpNummer INNER JOIN Verkoper ver ON v.VerkopersID = ver.GebruikersID WHERE v.Voorwerpnummer != $voorwerpnummer and VerkopersID IN
                                           (SELECT VerkopersID FROM Voorwerp WHERE Voorwerpnummer = $voorwerpnummer)");
     $MeerVanVerkoperQuery->execute([$id]);
     $MeerVanVerkoper = $MeerVanVerkoperQuery->fetchAll();
