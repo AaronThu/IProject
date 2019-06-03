c6b0eee2e6b5019cdc81f94b3d31146bf06fb065
AaronThunnissen <49440443+AaronThunnissen@users.noreply.github.com>
2019-06-03 16:04:10 +0200

    Add files via upload

---
+++ Website/verkooppagina.php
@@ -0,0 +1,69 @@
+<?php
+
+include_once "includes/functies.php";
+include_once "includes/database.php";
+include_once "includes/header.php";
+?>
+
+<body class="d-flex flex-column">
+    <div>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <h2 class="text-center">Product verkopen</h1>
+                </div>
+            </div>
+            <div class="row">         
+               
+                <div class="col-md-6 verkooppagina">
+                <form method="post" action="index.php"> 
+                <p class = "verkoophead"> Product specificaties </p>    
+                        <h6>Titel van je product</h6>
+                        <input type="text" class= "form-control inputforms" required>
+                        <h6>Beschrijving van je product</h6>
+                        <input type="text" class= "form-control inputforms" required>
+                        <h6>Afbeelding(en) van je product</h6>
+                        <input type="file" class= "form-control inputforms" name="bestand" required>
+                        <h6>Looptijd</h6>
+                        <select class = "form-control inputforms" name = "looptijd" required>
+                            <option value = "1">1 dag</option>
+                            <option value = "3">3 dagen</option>
+                            <option value = "5">5 dagen</option>
+                            <option value = "7">7 dagen</option>
+                            <option value = "9">9 dagen</option>
+                        </select>
+                </div>
+
+                
+                <div class="col-md-6 verkooppagina">
+                <p class = "verkoophead"> Prijs specificaties</p>
+                    <h6>Startprijs</h6>
+                    <input type="text" class= "form-control inputforms" required>
+                    <h6>Betalingsinstructies</h6>
+                    <input type="text" class= "form-control inputforms">
+                    <h6>Betalingswijze</h6>
+                    <select class = "form-control inputforms" name="betalingswijze" required>
+                        <option value = 'iDeal'>iDeal</option>
+                        <option value = 'Creditcard'>Creditcard</option>
+                    </select>
+                </div>
+
+            </div>
+        </div>
+        <div class="text-center" style="margin: 20px;">
+    <button class="btn btn-primary" type="submit" style="background-color: #a9976a;" name="veilen">Start veiling</button>
+    </div>
+    </form>
+    </div>
+    </div>
+    
+   
+    <script src="assets/js/jquery.min.js"></script>
+    <script src="assets/bootstrap/js/bootstrap.min.js"></script>
+</body>
+
+</html>
+
+<?php
+include_once "includes/footer.php";
+?>
