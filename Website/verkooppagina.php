13c56c92afb1bcc30034efd419d73aa32abe1631
Tymo <tymoschotanus@live.nl>
2019-06-04 14:39:49 +0200

    Update verkooppagina.php

--- Website/verkooppagina.php
+++ Website/verkooppagina.php
@@ -2,6 +2,15 @@
 
 include_once "includes/functies.php";
 include_once "includes/database.php";
+
+$locatieFouteLink = "Location: index.php";
+
+if (empty($_SESSION['Gebruikersnaam'])) {
+    $_SESSION['foutmelding'] = "Maak eerst een verkopers account om voorwerpen te kunnen verkopen";
+    header($locatieFouteLink);
+    return;
+}
+
 include_once "includes/header.php";
 ?>
 
@@ -13,10 +22,9 @@ include_once "includes/header.php";
                     <h2 class="text-center">Product verkopen</h1>
                 </div>
             </div>
-            <div class="verkooppaginarow">
-
-                <div class="col-md-6 verkooppagina">
-                    <form method="post" action="index.php">
+                    <form method="post" action="verkoop_systeem.php" enctype="multipart/form-data">
+                    <div class="verkooppaginarow">
+                    <div class="col-md-6 verkooppagina">
                         <p class="verkoophead"> Product specificaties </p>
                         <h6>Titel van je product</h6>
                         <input type="text" class="form-control inputforms" name="titel" required>
@@ -24,14 +32,12 @@ include_once "includes/header.php";
                         <h6>Afbeelding(en) van je product</h6>
                         <input type="file" class="form-control inputforms" name="bestand" required>
                         <h6>Beschrijving van je product</h6>
-                        <input type="text" class="form-control inputforms groot" name="beschrijving" required>
+                        <textarea rows=4 cols=50 class="form-control inputforms" name="beschrijving" required></textarea>
                 </div>
-
-
                 <div class="col-md-6 verkooppagina">
                     <p class="verkoophead"> Prijs specificaties</p>
                     <h6>Startprijs</h6>
-                    <input type="text" class="form-control inputforms" required>
+                    <input type="number" class="form-control inputforms" required>
            
                     <h6>Betalingswijze</h6>
                     <select class="form-control inputforms" name="betalingswijze" required>
@@ -39,15 +45,10 @@ include_once "includes/header.php";
                         <option value='Creditcard'>Creditcard</option>
                     </select>
                     <h6>Betalingsinstructies</h6>
-                    <input type="text" class="form-control inputforms groot">
+                    <input type="text" class="form-control inputforms">
                 </div>
-
-
-
                 <div class="col-md-6 verkooppagina">
                     <p class="verkoophead"> Overige specificaties</p>
-
-
                     <h6>Looptijd</h6>
                     <select class="form-control inputforms" name="looptijd" required>
                         <option value="1">1 dag</option>
@@ -56,26 +57,19 @@ include_once "includes/header.php";
                         <option value="7">7 dagen</option>
                         <option value="9">9 dagen</option>
                     </select>
-                    <h6> Jouw locatie</h6>
-                    <select class="form-control inputforms" name="locatie" required>
-                        <option value="#">#</option>
-                    </select>
+                    <h6>Locatie</h6>
+                    <input type=text class="form-control inputforms" name="locatie" required>
                     <h6> Verzendinstructie</h6>
-                    <input type="text" class="form-control inputforms groot">
+                    <input type="text" class="form-control inputforms">
                 </div>
 
                 <div class="col-md-6 verkooppagina">
                     <p class="verkoophead"> Rubriek</p>
                     <select class="form-control inputforms" name="parentrubriek" required>
                         <option value="#"></option>
-
-
                         <h6> Rubriek van product </h6>
                         <select class="form-control inputforms" name="rubriek" required></select>
-
                 </div>
-
-
             </div>
         </div>
         <div class="text-center" style="margin: 20px;">
@@ -86,8 +80,6 @@ include_once "includes/header.php";
     </div>
     <div class="spacing">
     </div>
-
-
     <script src="assets/js/jquery.min.js"></script>
     <script src="assets/bootstrap/js/bootstrap.min.js"></script>
 </body>
