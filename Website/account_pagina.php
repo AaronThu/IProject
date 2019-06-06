dc6ce00d5c89740a3ebba744a190907414fd9298
BeingBright <BrightonvandenEnd@gmail.com>
2019-06-06 16:04:41 +0200

    Update account_pagina.php

--- Website/account_pagina.php
+++ Website/account_pagina.php
@@ -93,10 +93,10 @@ $notifications = GetNotificaties($_SESSION['GebruikersID'],$_SESSION['SoortGebru
                         echo ("</div>");
                         foreach ($mijnVoorwerpen as $key => $value) {
                             echo ("<a href=\"voorwerppagina.php?voorwerpID=$value[Voorwerpnummer]\" class=\"row d-flex flex-wrap item\">");
-                            echo ("<h5 class=\"col-md-3\">[Titel]</h5>");
-                            echo ("<h5 class=\"col-md-3\">[Eindmoment]</h5>");
-                            echo ("<h5 class=\"col-md-3\">[Startprijs]</h5>");
-                            echo ("<h5 class=\"col-md-3\">[Verkoopprijs]</h5>");
+                            echo ("<h5 class=\"col-md-3\">$value[Titel]</h5>");
+                            echo ("<h5 class=\"col-md-3\">$value[Eindmoment]</h5>");
+                            echo ("<h5 class=\"col-md-3\">$value[Startprijs]</h5>");
+                            echo ("<h5 class=\"col-md-3\">$value[Verkoopprijs]</h5>");
                             echo ("</a>");
                         }
                     } else { ?>
